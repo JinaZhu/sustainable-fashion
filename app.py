b5bdfedc54760d044442658339bc4647b62a9663
Jina Zhu <jinazhu87@gmail.com>
2020-10-18 16:46:30 -0700

    handle 404 errors on app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 # from flask_sqlalchemy import flask_sqlalchemy
 
 app = Flask(__name__, static_folder='./client/build', static_url_path='/')
@@ -9,5 +10,10 @@ def home():
     return app.send_static_file('index.html')
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 80))
