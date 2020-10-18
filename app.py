286f68317ccd75ba557914fb945f14c71ef92383
Jina Zhu <jinazhu87@gmail.com>
2020-10-18 16:32:42 -0700

    deploy flask with react

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 # from flask_sqlalchemy import flask_sqlalchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./client/build', static_url_path='/')
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'hello world'
+    return app.send_static_file('index.html')
 
 
 if __name__ == "__main__":
