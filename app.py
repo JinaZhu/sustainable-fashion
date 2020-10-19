12b0ce5e37ef00f428b7ba36ac7d329b9689a745
Jina Zhu <jinazhu87@gmail.com>
2020-10-19 15:21:44 -0700

    create database and got database to run

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
+
+
 # from flask_sqlalchemy import flask_sqlalchemy
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
@@ -11,11 +13,11 @@ db = SQLAlchemy(app)
 
 
 class Survey(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return 'added'
+        return f"<message={self.content}>"
 
 
 @app.route('/', methods=['GET'])
@@ -23,12 +25,19 @@ def home():
     return app.send_static_file('index.html')
 
 
-@app.route('/hello', methods=['POST', 'GET'])
+@app.route('/hello', methods=['POST'])
 def index():
-    if request.method == "POST":
-        return jsonify('hello')
-    else:
-        return jsonify('hi')
+    message = request.get_json()
+
+    print('***********', message)
+    new_message = Survey(content=message)
+
+    try:
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify("success")
+    except:
+        return jsonify("there was an issue adding a message")
 
 
 @app.errorhandler(404)
@@ -37,4 +46,5 @@ def not_found(e):
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(host='0.0.0.0', debug=True, port=os.environ.get('PORT', 80))
---
+++ build/precache-manifest.e23ca3de8c85e59aaf06267a4d01074b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5e67de948f117962b82d0018cf5bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5c70af190d0be9ce69",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a26c5e6d7a6b1858937",
+    "url": "/static/js/2.342af312.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.342af312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5c70af190d0be9ce69",
+    "url": "/static/js/main.70de15aa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
