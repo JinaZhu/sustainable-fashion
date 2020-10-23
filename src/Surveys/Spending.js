c5ad57006b1306cc40a187970a4f0194344c1e5c
Jina Zhu <jinazhu87@gmail.com>
2020-10-22 18:16:56 -1000

    creat radio box logic

--- src/Surveys/Spending.js
+++ src/Surveys/Spending.js
@@ -1,49 +1,32 @@
 import React, { useState } from "react";
 import { SpendingOption, SpendingContainer, Question, Options } from "./styled";
 
+const spendingOptions = [
+  "$0-$49",
+  "$50-$99",
+  "$100-$499",
+  "$500-$999",
+  "$1000+",
+];
+
 const Spending = () => {
-  const [selectedSpendingAmount, setSelectedSpendingType] = useState("");
+  const [SpendingAmount, setSpendingType] = useState("");
+  console.log(SpendingAmount);
 
   return (
     <SpendingContainer>
       <Question>
         On average, how much do you spend on clothes each year?
       </Question>
-      <SpendingOption>
-        <Options>$0-$49</Options>
-      </SpendingOption>
-      <SpendingOption>
-        <Options>$50-$99</Options>
-      </SpendingOption>
-      <SpendingOption>
-        <Options>$100-$499</Options>
-      </SpendingOption>
-      <SpendingOption>
-        <Options>$500-$999</Options>
-      </SpendingOption>
-      <SpendingOption>
-        <Options>$1000+</Options>
-      </SpendingOption>
+      {spendingOptions.map((option) => {
+        return (
+          <SpendingOption key={option} onClick={() => setSpendingType(option)}>
+            <Options>{option}</Options>
+          </SpendingOption>
+        );
+      })}
     </SpendingContainer>
   );
-
-  //   return (
-  //     <SpendingContainer>
-
-  //       <label>On average, how much do you spend on clothes each year?</label>
-  //       <p>$0-$49</p>
-  //       <RadioButton type="radio" name="spending" value="$0-$49" />
-  //       <p>$50-$99</p>
-  //       <RadioButton type="radio" name="spending" value="$50-$99" />
-  //       <p>$100-$499</p>
-  //       <RadioButton type="radio" name="spending" value="$100-$499" />
-  //       <p>$500-$999</p>
-  //       <RadioButton type="radio" name="spending" value="$500-$999" />
-  //       <p>$1000+</p>
-  //       <RadioButton type="radio" name="spending" value="$1000" />
-  //       <button>Submit</button>
-  //     </SpendingContainer>
-  //   );
 };
 
 export default Spending;
