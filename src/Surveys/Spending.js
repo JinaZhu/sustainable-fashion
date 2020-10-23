8e1450f0e02a5953be933ebd9470100c58514f23
Jina Zhu <jinazhu87@gmail.com>
2020-10-22 18:00:02 -1000

    style spending survey

--- src/Surveys/Spending.js
+++ src/Surveys/Spending.js
@@ -1,23 +1,49 @@
-import React from "react";
-import { RadioButton, SpendingContainer } from "./styled";
+import React, { useState } from "react";
+import { SpendingOption, SpendingContainer, Question, Options } from "./styled";
 
 const Spending = () => {
+  const [selectedSpendingAmount, setSelectedSpendingType] = useState("");
+
   return (
     <SpendingContainer>
-      <p>On average, how much do you spend on clothes each year?</p>
-      <label>$0-$49</label>
-      <RadioButton type="radio" value="$0-$49" />
-      <label>$50-$99</label>
-      <RadioButton type="radio" value="$50-$99" />
-      <label>$100-$499</label>
-      <RadioButton type="radio" value="$100-$499" />
-      <label>$500-$999</label>
-      <RadioButton type="radio" value="$500-$999" />
-      <label>$1000+</label>
-      <RadioButton type="radio" value="$1000" />
-      <button>Submit</button>
+      <Question>
+        On average, how much do you spend on clothes each year?
+      </Question>
+      <SpendingOption>
+        <Options>$0-$49</Options>
+      </SpendingOption>
+      <SpendingOption>
+        <Options>$50-$99</Options>
+      </SpendingOption>
+      <SpendingOption>
+        <Options>$100-$499</Options>
+      </SpendingOption>
+      <SpendingOption>
+        <Options>$500-$999</Options>
+      </SpendingOption>
+      <SpendingOption>
+        <Options>$1000+</Options>
+      </SpendingOption>
     </SpendingContainer>
   );
+
+  //   return (
+  //     <SpendingContainer>
+
+  //       <label>On average, how much do you spend on clothes each year?</label>
+  //       <p>$0-$49</p>
+  //       <RadioButton type="radio" name="spending" value="$0-$49" />
+  //       <p>$50-$99</p>
+  //       <RadioButton type="radio" name="spending" value="$50-$99" />
+  //       <p>$100-$499</p>
+  //       <RadioButton type="radio" name="spending" value="$100-$499" />
+  //       <p>$500-$999</p>
+  //       <RadioButton type="radio" name="spending" value="$500-$999" />
+  //       <p>$1000+</p>
+  //       <RadioButton type="radio" name="spending" value="$1000" />
+  //       <button>Submit</button>
+  //     </SpendingContainer>
+  //   );
 };
 
 export default Spending;
--- src/Surveys/styled.js
+++ src/Surveys/styled.js
@@ -4,6 +4,14 @@ export const SurveysContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100vh;
+  background-color: #0f4c81;
+`;
+
+export const Question = styled.p`
+  font-family: "Gravitas One", cursive;
+  font-size: 25px;
+  color: white;
 `;
 
 export const SpendingContainer = styled.div`
@@ -11,13 +19,24 @@ export const SpendingContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  border-left: 1px solid white;
+  border-right: 1px solid white;
+  width: 500px;
+  padding: 5px;
 `;
 
-export const RadioButton = styled.input`
+export const Options = styled.p`
+  font-family: "Montserrat", sans-serif;
+`;
+
+export const SpendingOption = styled.div`
+  border: 1px solid white;
+  width: 10rem;
   margin: 5px;
-  cursor: pointer;
-  font-size: 22px;
-  height: 25px;
-  width: 25px;
-  display: block;
+  color: white;
+
+  &:hover {
+    background-color: white;
+    color: #0f4c81;
+  }
 `;
