555b4880ca731087e5b9c0c815d6064f6bcd0733
Jina Zhu <jinazhu87@gmail.com>
2020-11-10 15:51:24 -1000

    rebug-build

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
 
-const DEBUG = process.env.NODE_ENV === "development" ? true : false;
-
-const PREFIX = DEBUG ? "http://0.0.0.0:8000" : "";
-
-const api_call = PREFIX + "/api/hello";
-
 const Homepage = () => {
   const [backendData, setBackendData] = useState("");
   const [message, setMessage] = useState("");
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 export const DEBUG = process.env.NODE_ENV === "development" ? true : false;
-export const PREFIX = DEBUG ? "http://0.0.0.0:8000" : "";
+export const PREFIX = DEBUG ? "http://0.0.0.0:8000" : "https://sustainable-fashion.herokuapp.com/";
