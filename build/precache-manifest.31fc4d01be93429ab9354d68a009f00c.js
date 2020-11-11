820fea340a3ed0059602036aa63425c3e91f921c
Jina Zhu <jinazhu87@gmail.com>
2020-11-10 20:03:04 -1000

    build app

--- build/precache-manifest.31fc4d01be93429ab9354d68a009f00c.js
+++ build/precache-manifest.31fc4d01be93429ab9354d68a009f00c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92de0ae282837b444a1e84eaf66f0a8a",
+    "revision": "4777f6547c02b95f28e2beab9a2a553c",
     "url": "/index.html"
   },
   {
-    "revision": "10c0eb5bd4d6821bfa55",
+    "revision": "1fbbdec7a6122b2f605d",
     "url": "/static/css/main.777b383b.chunk.css"
   },
   {
-    "revision": "a72da832959e593d8d58",
-    "url": "/static/js/2.9e0dd090.chunk.js"
+    "revision": "85cdd2351c33f87b79d4",
+    "url": "/static/js/2.85f28333.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/static/js/2.9e0dd090.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.85f28333.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "10c0eb5bd4d6821bfa55",
-    "url": "/static/js/main.42293c71.chunk.js"
+    "revision": "1fbbdec7a6122b2f605d",
+    "url": "/static/js/main.51dc5da9.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
